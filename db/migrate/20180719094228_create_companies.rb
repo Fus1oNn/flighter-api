608ef3d51255c022d7fa8b5472edac1c9430b7f9
Tomislav Skokovic <tomislav.skokovic@gmail.com>
2018-07-19 12:05:14 +0200

    Add unique constraint

--- db/migrate/20180719094228_create_companies.rb
+++ db/migrate/20180719094228_create_companies.rb
@@ -1,7 +1,7 @@
 class CreateCompanies < ActiveRecord::Migration[5.2]
   def change
     create_table :companies do |t|
-      t.string :name, null: false
+      t.string :name, null: false, unique: true
 
       t.timestamps null: false
     end
