2cbb9351896042f6892ef143ee19505694025059
Tomislav Skokovic <tomislav.skokovic@gmail.com>
2018-07-23 18:11:58 +0200

    Fix ActiveRecord 404 error name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
-  rescue_from ActiveRecord::NotFoundError, with: :not_found
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   def not_found
     render json: { errors: { resource: ['does not exist'] } },
